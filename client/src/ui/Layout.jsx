a484d4a6b584be1dd62745084462a4798df9ad68
Mark Golubtsov <glmarkgl4@gmail.com>
2024-11-23 18:50:24 +0300

    Добавлен контент.

--- client/src/ui/Layout.jsx
+++ client/src/ui/Layout.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import AppHeader from './AppHeader';
 import {Layout as AntdLayout} from 'antd';
+
 
 function Layout() {
     return (
         <AntdLayout>
-            <AppHeader />
-            <Outlet />
+            <AppHeader/>
+            <AntdLayout.Content>
+                <Outlet/>
+            </AntdLayout.Content>
         </AntdLayout>
     );
 }
