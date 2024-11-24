b0bae5bb731e44acf70e5ff0fd22e784cdfc2dde
Mark Golubtsov <glmarkgl4@gmail.com>
2024-11-24 17:54:17 +0300

    Доработана форма создания вопроса.

--- client/src/ui/question/QuestionFormPage.jsx
+++ client/src/ui/question/QuestionFormPage.jsx
@@ -1,35 +1,45 @@
-import {Button, Col, Form, Input, notification, Row, Select} from "antd";
+import {Button, Col, Form, Input, notification, Row, Select, Typography} from "antd";
 import {useStore} from "../../store/useStore";
 import categoryService from "../../api/categoryService";
 import questionService from "../../api/questionService";
 import {useAuth} from "../../auth/AuthContext";
 import {useQuestionsNavigation} from "./useQuestionsNavigation";
+import {REQUIRED_RULE} from "../../util/validation-rules";
 
 export const QuestionFormPage = () => {
     const {data: categories} = useStore(categoryService.getAllCategories, []);
     const {user} = useAuth()
     const categorySelectOptions = categories?.map(it => ({label: it.name, value: it.id}));
-    const { goToQuestionPage} = useQuestionsNavigation();
+    const {goToQuestionPage} = useQuestionsNavigation();
 
     return (
-        <Row>
-            <Col offset={6} span={12}>
-                <Form onFinish={handleSubmit} layout='vertical'>
-                    <Form.Item name='title' label='Заголовок'>
-                        <Input/>
-                    </Form.Item>
-                    <Form.Item name='content' label='Тело вопроса'>
-                        <Input.TextArea/>
-                    </Form.Item>
-                    <Form.Item name='category_id' label='Категория'>
-                        <Select options={categorySelectOptions}/>
-                    </Form.Item>
-                    <Button size='large' type='primary' htmlType='submit'>
-                        Создать
-                    </Button>
-                </Form>
-            </Col>
-        </Row>
+        <>
+            <Row>
+                <Col offset={6} span={12}>
+                    <Typography.Title level={2}>
+                        Создание вопроса
+                    </Typography.Title>
+                </Col>
+            </Row>
+            <Row>
+                <Col offset={6} span={12}>
+                    <Form onFinish={handleSubmit} layout='vertical'>
+                        <Form.Item required rules={[REQUIRED_RULE]} name='title' label='Заголовок'>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item required rules={[REQUIRED_RULE]} name='content' label='Тело вопроса'>
+                            <Input.TextArea/>
+                        </Form.Item>
+                        <Form.Item required rules={[REQUIRED_RULE]} name='category_id' label='Категория'>
+                            <Select options={categorySelectOptions}/>
+                        </Form.Item>
+                        <Button size='large' type='primary' htmlType='submit'>
+                            Создать
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </>
     )
 
     function handleSubmit(data) {
