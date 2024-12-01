61f0cbda3de3ccc4fecf9405448b43b1f1641131
Mark Golubtsov <glmarkgl4@gmail.com>
2024-12-01 17:13:48 +0300

    Удаление лишнего.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,6 @@ import Login from './ui/Login';
 import Register from './ui/Register';
 import {AuthProvider} from './auth/AuthContext';
 import Home from './ui/Home';
-import HomeAdmin from './ui/admin/Home';
-import AuthGuard from './auth/AuthGuard';
 import {AxiosInterceptor} from './api/axios';
 import NotFound from './ui/NotFound';
 import {ConfigProvider} from 'antd';
@@ -43,12 +41,6 @@ root.render(
                             <Route path="questions/:id" element={<QuestionPage/>}/>
                             <Route path='questions/create-question' element={<QuestionFormPage/>}/>
                             <Route path='charts' element={<ChartsPage/>}/>
-                        </Route>
-
-                        <Route element={<AuthGuard/>}>
-                            <Route path="/admin" element={<Layout/>}>
-                                <Route index element={<HomeAdmin/>}/>
-                            </Route>
                         </Route>
 
                         <Route path='*' element={<NotFound/>}/>
